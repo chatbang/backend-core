dbfc755978ce8964c8b651ca708ea001b920d00a
Victor F <nftvictor@gmail.com>
2023-06-26 23:49:04 +0800

    Save split text chunks into pinecone.

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask import Flask, request
 from dotenv import load_dotenv
 from config import config
 from flask_cors import CORS
+from langchain.vectorstores.pinecone import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+import pinecone
 from processor import processor
 
+OPEN_AI_KEY = os.getenv('OPEN_AI_KEY')
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_ENV = os.getenv('PINECONE_ENV')
+PINECONE_INDEX_NAME = os.getenv('PINECONE_INDEX_NAME')
 
 # 配置日志信息
 config.setup_log()
@@ -53,9 +60,17 @@ def file_processor():
 
         # get the text chunks
         text_chunks = processor.get_text_chunks(text)
-        print(text_chunks)
 
-        # TODO save the text chunks to pinecone
+        # save the text chunks to pinecone
+        metadatas = [{'fileName': file.filename, 'page': i} for i in range(len(text_chunks))]
+        embeddings = OpenAIEmbeddings(openai_api_key=OPEN_AI_KEY)
+        Pinecone.from_texts(
+            text_chunks,
+            embeddings,
+            index_name=PINECONE_INDEX_NAME,
+            metadatas=metadatas,
+            namespace='default'
+        )
 
         return {'code': 200, 'message': 'file processed successfully'}
 
@@ -69,8 +84,15 @@ def chatgpt_completion():
     return {'code': 200,  'message': 'success', 'data': {'question': question, 'answer': answer}}
 
 
+def pinecone_setup():
+    pinecone.init(
+        api_key=PINECONE_API_KEY,
+        environment=PINECONE_ENV,
+    )
+
 def main():
     load_dotenv()
+    pinecone_setup()
     app.run(port=os.getenv('PORT') or 10020, debug=True, host='0.0.0.0')
     app.logger.info('port')
 
